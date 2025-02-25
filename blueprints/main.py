3ef7668a33bdd78c922263f22c5a201c23d7658c
resquez <83383092+resquez@users.noreply.github.com>
2025-02-25 16:57:25 +0900

    Delete blueprints directory

--- blueprints/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, render_template
-from blueprints.utils import get_db_connection
-
-main_bp = Blueprint('main', __name__, url_prefix='/main')
-
-# 📌 예약 페이지 라우트
-@main_bp.route('/')
-def main():
-    return render_template('reservation.html')

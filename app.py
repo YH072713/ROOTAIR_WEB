43bbd73f4e8cc60078e9d2f473ac4cf82197acf8
resquez <83383092+resquez@users.noreply.github.com>
2025-02-25 17:06:13 +0900

    initial files

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from blueprints import blueprints  # `__init__.py`에서 가져옴
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '1234'
+
+# Blueprint 등록
+for bp in blueprints:
+    app.register_blueprint(bp)
+
+# 현재 등록된 모든 URL 확인 (디버깅 용도)
+print(app.url_map)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)

f0dccfea9a758ede2c8d2733e29dc96493820624
justssom <127735439+justssom@users.noreply.github.com>
2025-02-27 11:23:38 +0900

    Update main.py

--- blueprints/main.py
+++ blueprints/main.py
@@ -3,7 +3,6 @@ from blueprints.utils import get_db_connection
 
 main_bp = Blueprint('main', __name__, url_prefix='/main')
 
-# 📌 예약 페이지 라우트
 @main_bp.route('/')
 def main():
-    return render_template('reservation.html')
+    return render_template('main/main.html')

8d44b3e6c2e9c21a7fddcc1cb28ca985446ca0aa
ioop353 <ioop7799@gmail.com>
2025-02-28 17:40:49 +0900

    2차 정리본

--- static/js/member/member_signup.js
+++ static/js/member/member_signup.js
@@ -1,12 +1,3 @@
-// 헤더
-const togglebtn = document.querySelector('.navbar_togglebtn');
-const menu = document.querySelector('.navbar_menu');
-const member = document.querySelector('.navbar_member');
-
-togglebtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    member.classList.toggle('active');
-});
 
 function searchAddress() {
     new daum.Postcode({
--- static/js/notices/notices.js
+++ static/js/notices/notices.js
@@ -1,11 +1,3 @@
-const togglebtn = document.querySelector('.navbar_togglebtn');
-const menu = document.querySelector('.navbar_menu');
-const member = document.querySelector('.navbar_member');
-
-togglebtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    member.classList.toggle('active');
-});
 
 document.addEventListener("DOMContentLoaded", function () {
     const itemsPerPage = 5;
